a79417be54843665e8b9b3ea5f46844513a83efb
Panda0412 <nasb_99@mail.ru>
2020-08-24 12:32:13 +0300

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
-import AppScreens from './components/AppScreens';
+// import AppScreens from './components/AppScreens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Icon} from 'native-base';
+import Quests from './components/Quests';
+import MapList from './components/MapList';
+import PersonList from './components/PersonList';
+
+const Tab = createBottomTabNavigator();
 
 const App = (): React.ReactElement => {
-  return <AppScreens />;
+  return (
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Map">
+          <Tab.Screen
+              name="Quest"
+              options={{
+                  title: 'КВЕСТЫ',
+                  tabBarIcon: (): React.ReactElement=>{
+                      return <Icon type="FontAwesome5" name="route" />;
+                  }}}
+              component={Quests} />
+          <Tab.Screen
+              name="Map"
+              options={{
+                  title: 'КАРТА',
+                  tabBarIcon: (): React.ReactElement=>{
+                      return <Icon type="FontAwesome5" name="map-marked-alt" />;
+                  }}}
+              component={MapList} />
+          <Tab.Screen
+              name="Person"
+              options={{
+                  title: 'ПРОФИЛЬ',
+                  tabBarIcon: (): React.ReactElement=>{
+                      return <Icon type="FontAwesome5" name="user" />;
+                  }}}
+              component={PersonList} />
+        </Tab.Navigator>
+      </NavigationContainer>
+  );
 };
 
 export default App;
