f887192ea3e080b4d69503abb89473984c74995a
Nikita Melnikov <nikmel2803@gmail.com>
2020-09-28 11:44:34 +0300

    remove unnecessary code

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/**
- * @format
- */
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
@@ -8,6 +5,5 @@ import { name as appName } from './app.json';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
 MapboxGL.setAccessToken('pk.eyJ1IjoibmlrbWVsMjgwMyIsImEiOiJjazR2cTc0M3MwcHpyM2ptcnhveWFxeGN1In0.vi88gMCid-SNTmuo_gkasg');
-MapboxGL.setConnected(true);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import TabsNavigator from './components/TabsNavigator';
-import Map from "./components/Map";
 
 const App = (): React.ReactElement => {
   return <TabsNavigator/>;
