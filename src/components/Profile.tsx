cc7bd4970ff80de5dd302ce2d00747cebbc9b6c6
Nikita Melnikov <nikmel2803@gmail.com>
2020-11-08 14:48:03 +0300

    add google sign in button

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,6 +4,13 @@ import { useTranslation } from 'react-i18next';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { ProfileStackParamList } from './AppNavigator';
 import { useNavigation } from '@react-navigation/native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes
+} from '@react-native-community/google-signin';
+
+GoogleSignin.configure();
 
 /**
  * Type with props of screen 'Main' in QuestsStackScreen
@@ -138,6 +145,29 @@ export default function Profile(): React.ReactElement {
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>{t('profile.cards')}</Text>
       </TouchableOpacity>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={async () => {
+          try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            console.log(userInfo)
+            // this.setState({ userInfo });
+          } catch (error) {
+            console.log(error)
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+              // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+              // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+              // play services not available or outdated
+            } else {
+              // some other error happened
+            }
+          }
+        }}
+      />
     </ScrollView>
   );
 }
