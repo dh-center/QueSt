275b73fe129b8b5d48516c733c918508859f4ad7
Panda0412 <nasb_99@mail.ru>
2020-11-25 01:24:13 +0300

    Test screen

---
+++ src/components/TestView.tsx
@@ -0,0 +1,176 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  SafeAreaView
+} from 'react-native';
+import colors from '../styles/colors';
+import Question from '../images/question.svg';
+import RightAnswer from '../images/rightAnswer.svg';
+import WrongAnswer from '../images/wrongAnswer.svg';
+import Voice from '../images/voice.svg';
+import Next from '../images/nextButton.svg';
+
+const testQuestion = {
+  question: 'Как называл Маяковский упадочное настроение среди молодежи?',
+  answers: ['Солжиница', 'Есенищина', 'Гумильвица', 'Сологубщина'],
+  correctAnswerIndex: 1,
+};
+
+const answerButton = StyleSheet.create({
+  button: {
+    minHeight: 60,
+    marginBottom: 10,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    lineHeight: 22,
+  },
+});
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: colors.white,
+    height: '100%',
+  },
+  view: {
+    backgroundColor: colors.fond,
+  },
+  headerBlock: {
+    backgroundColor: colors.white,
+    paddingTop: 50,
+    paddingRight: 15,
+    paddingBottom: 30,
+    paddingLeft: 15,
+    borderBottomRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    elevation: 4,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  headerImages: {
+    width: '100%',
+    paddingRight: 15,
+    paddingBottom: 30,
+    paddingLeft: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  invisible: {
+    opacity: 0,
+  },
+  answersView: {
+    paddingTop: 30,
+    paddingRight: 15,
+    paddingBottom: 20,
+    paddingLeft: 15,
+  },
+  activeAnswerButton: {
+    ...answerButton.button,
+    backgroundColor: colors.white,
+    elevation: 10,
+  },
+  disabledAnswerButton: {
+    ...answerButton.button,
+    backgroundColor: colors.white,
+  },
+  rightAnswerButton: {
+    ...answerButton.button,
+    backgroundColor: colors.white,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: colors.green,
+  },
+  rightAnsweredButton: {
+    ...answerButton.button,
+    backgroundColor: colors.green,
+  },
+  wrongAnsweredButton: {
+    ...answerButton.button,
+    backgroundColor: colors.red,
+  },
+  headerText: {
+    color: colors.black,
+    fontSize: 22,
+    lineHeight: 22,
+  },
+  blackAnswerText: {
+    ...answerButton.buttonText,
+    color: colors.black,
+  },
+  whiteAnswerText: {
+    ...answerButton.buttonText,
+    color: colors.white,
+  },
+  next: {
+    height: 64,
+    width: 64,
+    marginBottom: 30,
+    borderRadius: 32,
+    elevation: 10,
+    alignSelf: 'center',
+  },
+});
+
+/**
+ * Displays settings screen
+ */
+export default function Profile(): React.ReactElement {
+  const [result, setResult] = useState<boolean>();
+  const [selectedAnswer, setSelectedAnswer] = useState<number>();
+
+  return (
+    <SafeAreaView style={styles.body}>
+      <ScrollView style={styles.view}>
+        <View style={styles.headerBlock}>
+          <View style={styles.headerImages}>
+            <Voice style={styles.invisible}/>
+            {(result == undefined) && <Question/>}
+            {(result == true) && <RightAnswer/>}
+            {(result == false) && <WrongAnswer/>}
+            <Voice/>
+          </View>
+          <Text style={styles.headerText}>{testQuestion.question}</Text>
+        </View>
+        <View style={styles.answersView}>
+          {
+            testQuestion.answers.map((answer, index) => {
+              return (
+                <TouchableOpacity
+                  disabled={!(result == undefined)}
+                  style={
+                    (result == undefined) ? styles.activeAnswerButton
+                      : (result && index == testQuestion.correctAnswerIndex) ? styles.rightAnsweredButton
+                        : (!result && index == testQuestion.correctAnswerIndex) ? styles.rightAnswerButton
+                          : (!result && index == selectedAnswer) ? styles.wrongAnsweredButton
+                            : styles.disabledAnswerButton
+                  }
+                  key={index.toString()}
+                  activeOpacity={0.5}
+                  onPress={(): void => {
+                    (index == testQuestion.correctAnswerIndex)
+                      ? setResult(true)
+                      : setResult(false);
+                    setSelectedAnswer(index);
+                  }}
+                >
+                  <Text style={
+                    (result && index == testQuestion.correctAnswerIndex) || (!result && index == selectedAnswer)
+                      ? styles.whiteAnswerText : styles.blackAnswerText
+                  }>{answer}</Text>
+                </TouchableOpacity>
+              );
+            })
+          }
+        </View>
+        {(result != undefined) && <TouchableOpacity><Next style={styles.next}/></TouchableOpacity>}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export default {
+  white: '#FFF',
+  blueBlack: '#55556B',
+  yellow: '#F6E864',
+  lightBlue: '#68C6DF',
+  green: '#5BC378',
+  red: '#ef6b59',
+  black: '#222222',
+  fond: 'rgba(142,178,188,0.1)',
+};
