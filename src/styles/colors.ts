a8e31086fea768d33a1de22e32a2b20b038b8032
Nikita Melnikov <nikmel2803@gmail.com>
2020-12-02 23:23:35 +0300

    remove unused colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -5,7 +5,7 @@ enum Colors {
   /**
    * Background color
    */
-  BACKGROUND = 'rgb(244, 247, 248)',
+  BACKGROUND = '#f4f7f8',
 
   /**
    * Black color
@@ -44,14 +44,3 @@ enum Colors {
 }
 
 export default Colors;
-
-export const sdfsd=  {
-  white: '#FFF',
-  blueBlack: '#55556B',
-  yellow: '#F6E864',
-  lightBlue: '#68C6DF',
-  green: '#5BC378',
-  red: '#ef6b59',
-  black: '#222222',
-  fond: 'rgba(142,178,188,0.1)',
-};
