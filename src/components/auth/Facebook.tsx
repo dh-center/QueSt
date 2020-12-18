ee37192e80d0d2b9e5e4d2bff6e1919b7216568f
Nikita Melnikov <nikmel2803@gmail.com>
2020-12-18 20:51:12 +0300

    Facebook and Google sign in ench

--- src/components/auth/Facebook.tsx
+++ src/components/auth/Facebook.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import FacebookLogo from '../../images/facebook.svg';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import authController from '../../controllers/authController';
+import { useTranslation } from 'react-i18next';
 
 /**
  * Button for performing authorization via Facebook
  */
 export default function FacebookAuth(): React.ReactElement {
+  const { t } = useTranslation();
   const signInWithFacebook = async (): Promise<void> => {
-    await authController.authWithFacebook();
+    try {
+      await authController.authWithFacebook();
+    } catch (e) {
+      Alert.alert(t([`errors.${e.message}`, 'errors.unspecific']));
+    }
   };
 
   return (
--- src/components/auth/Google.tsx
+++ src/components/auth/Google.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import GoogleLogo from '../../images/google.svg';
 import { GoogleSignin } from '@react-native-community/google-signin';
 import { OAUTH_WEB_CLIENT_ID } from '@env';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import authController from '../../controllers/authController';
+import { useTranslation } from 'react-i18next';
 
 GoogleSignin.configure({
   webClientId: OAUTH_WEB_CLIENT_ID,
@@ -14,8 +15,13 @@ GoogleSignin.configure({
  * Button for performing authorization via Google
  */
 export default function GoogleAuth(): React.ReactElement {
+  const { t } = useTranslation();
   const signInWithGoogle = async (): Promise<void> => {
-    await authController.authWithGoogle();
+    try {
+      await authController.authWithGoogle();
+    } catch (e) {
+      Alert.alert(t([`errors.${e.message}`, 'errors.unspecific']));
+    }
   };
 
   return (
