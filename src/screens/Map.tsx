e2cb3c41d3fcbf5507fd14089a1c09c1bc539fdc
Nikita Melnikov <nikmel2803@gmail.com>
2020-12-25 18:49:19 +0300

    Bottom sheet (#38)
    * add bottom sheet
    
    * fix linter
    
    * handle position inside

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, StyleSheet } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import { MAPBOX_ACCESS_TOKEN } from '@env';
 import TestView from '../components/TestView';
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { TabParamList } from '../navigation/mainTabs';
+import { Modalize } from 'react-native-modalize';
 
 /**
  * Type with props of screen 'Map' in BottomTabNavigator
@@ -31,11 +32,19 @@ const styles = StyleSheet.create({
  *
  * @param props - props for component rendering
  */
-export default function MapScreen({ route }: Props): React.ReactElement {
+export default function MapScreen({ route: _route }: Props): React.ReactElement {
+  const modalizeRef = useRef<Modalize>(null);
+
   return (
     <View style={styles.page}>
       <MapboxGL.MapView style={styles.map} />
-      {route.params?.questId && <TestView />}
+      <Modalize
+        handlePosition={'inside'}
+        ref={modalizeRef}
+        alwaysOpen={150}
+      >
+        <TestView/>
+      </Modalize>
     </View>
   );
 }
