c312a32345fe04cd3886d31996be2ebdfdd80e84
Nikita Melnikov <nikmel2803@gmail.com>
2020-12-27 17:37:36 +0300

    Fetch and render quest data (#40)
    * refactor map view and fetch quest data
    
    * add switch case
    
    * render text and set current targer
    
    * display current targer
    
    * update docs
    
    * add docs to quest data types
    
    * fix linter
    
    * remove log
    
    * fix error
    
    * fix review comments

---
+++ src/components/questBlocks/LocationInstance.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import { graphql, QueryRenderer } from 'react-relay';
+import enviroment from '../../enviroment';
+import { LocationInstanceQuestBlockQuery } from './__generated__/LocationInstanceQuestBlockQuery.graphql';
+
+/**
+ * Props for QuestLocationInstanceBlock component
+ */
+interface QuestLocationInstanceBlockProps {
+  /**
+   * Location instance id to fetch and display
+   */
+  locationInstanceId: string;
+}
+
+/**
+ * Renders location pointer on map
+ *
+ * @param props - props for component rendering
+ */
+export default function QuestLocationInstanceBlock({ locationInstanceId }: QuestLocationInstanceBlockProps): React.ReactElement | null {
+  return (
+    <QueryRenderer<LocationInstanceQuestBlockQuery>
+      environment={enviroment}
+      query={graphql`
+        query LocationInstanceQuestBlockQuery($id: GlobalId!) {
+            locationInstance(id: $id) {
+              id
+              name
+              location {
+                id
+                latitude
+                longitude
+              }
+            }
+        }
+      `}
+      render={({ error, props }) => {
+        if (error) {
+          console.error(error);
+
+          return null;
+        }
+
+        if (!props || !props.locationInstance) {
+          return null;
+        }
+
+        const location = props.locationInstance.location;
+
+        if (!location.latitude || !location.longitude) {
+          return null;
+        }
+
+        return (
+          <MapboxGL.PointAnnotation
+            id={props.locationInstance.id}
+            title={props.locationInstance.name || ''}
+            coordinate={[location.longitude, location.latitude]}
+          />
+        );
+      }}
+      variables={{
+        id: locationInstanceId,
+      }}
+    />
+  );
+}
--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,11 +1,11 @@
-import React, { useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import { MAPBOX_ACCESS_TOKEN } from '@env';
-import TestView from '../components/TestView';
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { TabParamList } from '../navigation/mainTabs';
-import { Modalize } from 'react-native-modalize';
+import QuestWalkthroughRenderer from '../components/QuestWalkthroughRenderer';
+import styled from 'styled-components/native';
+import MapView from '../components/MapView';
 import Colors from '../styles/colors';
 
 /**
@@ -15,54 +15,26 @@ type Props = BottomTabScreenProps<TabParamList, 'Map'>;
 
 MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    height: '100%',
-    width: '100%',
-  },
-  testModal: {
-    overflow: 'hidden',
-    backgroundColor: Colors.Background,
-  },
-});
+const Page = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${Colors.Background};
+`;
 
 /**
  * Renders map for quests
  *
  * @param props - props for component rendering
  */
-export default function MapScreen({ route: _route }: Props): React.ReactElement {
-  const modalizeRef = useRef<Modalize>(null);
+export default function MapScreen({ route }: Props): React.ReactElement {
+  if (route.params?.questId) {
+    return <QuestWalkthroughRenderer questId={route.params.questId}/>;
+  }
 
   return (
-    <View style={styles.page}>
-      <Modalize
-        handlePosition={'inside'}
-        ref={modalizeRef}
-        alwaysOpen={150}
-        modalStyle={styles.testModal}
-      >
-        <TestView/>
-      </Modalize>
-      <MapboxGL.MapView style={styles.map}>
-        <MapboxGL.Camera
-          defaultSettings={{
-            centerCoordinate: [30.3462, 59.9296],
-            zoomLevel: 9,
-          }}
-          maxBounds={{
-            ne: [31.263740364566985, 60.282501691026226],
-            sw: [29.281524984914313, 59.62023377214044],
-          }}
-          minZoomLevel={8.5}
-        />
-      </MapboxGL.MapView>
-    </View>
+    <Page>
+      <MapView/>
+    </Page>
   );
 }
