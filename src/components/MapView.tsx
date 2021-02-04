156a8ce7ca7094e2fbb97076a3d297144cc9d1cb
Anastasiia Bugaeva <=9$f/_w/-pkUa7H>
2021-02-04 20:17:02 +0300

    fix gradient for ios

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -11,6 +11,13 @@ const MapboxView = styled(MapboxGL.MapView)`
   width: 100%;
 `;
 
+const HeaderGradient = styled(LinearGradient)<{height: number}>`
+  height: ${(props): number => props.height}px;
+  width: 100%;
+  position: absolute;
+  top: 0;
+`;
+
 /**
  * Component that renders map
  *
@@ -33,8 +40,24 @@ export default function MapView(props: PropsWithChildren<unknown>): React.ReactE
   }, [ t ]);
 
   return (
-    <MapboxView>
-      <LinearGradient
+    <>
+      <MapboxView>
+        <MapboxGL.Camera
+          defaultSettings={{
+            centerCoordinate: [30.3462, 59.9296],
+            zoomLevel: 9,
+          }}
+          maxBounds={{
+            ne: [31.263740364566985, 60.282501691026226],
+            sw: [29.281524984914313, 59.62023377214044],
+          }}
+          minZoomLevel={8.5}
+        />
+        {hasPermission && <MapboxGL.UserLocation/>}
+        {props.children}
+      </MapboxView>
+      <HeaderGradient
+        height={insets.top + 40}
         start={{
           x: 0.5,
           y: 0,
@@ -45,21 +68,7 @@ export default function MapView(props: PropsWithChildren<unknown>): React.ReactE
         }}
         locations={[0.3, 1]}
         colors={['#FFF', 'rgba(255, 255, 255, 0)']}
-        style={{ height: insets.top + 40 }}
       />
-      <MapboxGL.Camera
-        defaultSettings={{
-          centerCoordinate: [30.3462, 59.9296],
-          zoomLevel: 9,
-        }}
-        maxBounds={{
-          ne: [31.263740364566985, 60.282501691026226],
-          sw: [29.281524984914313, 59.62023377214044],
-        }}
-        minZoomLevel={8.5}
-      />
-      {hasPermission && <MapboxGL.UserLocation/>}
-      {props.children}
-    </MapboxView>
+    </>
   );
 }
