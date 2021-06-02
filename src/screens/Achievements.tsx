c1da7f7a58ba73aebd04edf2b0c85c9370d999df
Panda0412 <nasb_99@mail.ru>
2021-06-02 17:32:01 +0300

    switch tabs

--- src/screens/Achievements.tsx
+++ src/screens/Achievements.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BlueCircle15 from '../images/blueCircle15.svg';
 import styled from 'styled-components/native';
 import { StyledFonts } from '../styles/textStyles';
@@ -10,7 +10,7 @@ import Emphasis from '../images/emphasis.svg';
 import { StackScreenProps } from '@react-navigation/stack';
 import { ProfileStackParamList } from '../navigation/profileStack';
 import ListButton from '../components/ListButton';
-import { ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
 
 type Props = StackScreenProps<ProfileStackParamList, 'Achievements'>;
 
@@ -43,24 +43,43 @@ const Title = styled.Text`
   color: ${Colors.Black};
 `;
 
-const TabView = styled.View`
+const TabView = styled.TouchableOpacity`
   padding: 0 25px 4px 15px;
 `;
 
-const BasicText = styled.Text<{inactive?: boolean, active?: boolean}>`
+const BasicText = styled.Text<{active?: boolean}>`
   ${props => props.active ? StyledFonts.uiWebMedium : StyledFonts.uiWebRegular};
-  ${props => props.inactive && 'opacity: 0.6'};
+  ${props => props.active === false && 'opacity: 0.6'};
   font-size: 18px;
   line-height: 22px;
   color: ${Colors.Black};
 `;
 
-const AchievementsView = styled(ScrollView).attrs(() => ({
+const AchievementsFlatList = styled(FlatList).attrs(() => ({
   contentContainerStyle: {
     paddingTop: 27,
     paddingHorizontal: 15,
   },
 }))``;
+
+const achievementsList = [
+  {
+    name: 'Друг Достоевского',
+    progress: 85,
+  },
+  {
+    name: 'Петербургская интеллигенция',
+    progress: 30,
+  },
+  {
+    name: 'История наводнений в Санкт-Петербурге',
+    progress: 70,
+  },
+  {
+    name: 'Начало положено!',
+    progress: 100,
+  },
+];
 
 /**
  * Displays achievements screen
@@ -70,6 +89,7 @@ const AchievementsView = styled(ScrollView).attrs(() => ({
 export default function AchievementsScreen({ navigation }: Props): React.ReactElement {
   const { t } = useTranslation();
   const tabBarHeight = useTabBarHeight();
+  const [currentTab, setCurrentTab] = useState(1);
 
   return (
     <Body tabBarHeight={tabBarHeight}>
@@ -81,23 +101,39 @@ export default function AchievementsScreen({ navigation }: Props): React.ReactEl
         <Title>{t('profile.achievements')}</Title>
       </Row>
       <Row>
-        <TabView>
-          <BasicText active>{t('achievements.all')}</BasicText>
-          <Emphasis/>
+        <TabView activeOpacity={1} onPress={() => setCurrentTab(1)}>
+          <BasicText active={currentTab === 1}>{t('achievements.all')}</BasicText>
+          {currentTab === 1 && <Emphasis/>}
         </TabView>
-        <TabView>
-          <BasicText inactive>{t('achievements.received')}</BasicText>
+        <TabView activeOpacity={1} onPress={() => setCurrentTab(2)}>
+          <BasicText active={currentTab === 2}>{t('achievements.received')}</BasicText>
+          {currentTab === 2 && <Emphasis/>}
         </TabView>
-        <TabView>
-          <BasicText inactive>{t('achievements.process')}</BasicText>
+        <TabView activeOpacity={1} onPress={() => setCurrentTab(3)}>
+          <BasicText  active={currentTab === 3}>{t('achievements.process')}</BasicText>
+          {currentTab === 3 && <Emphasis/>}
         </TabView>
       </Row>
-      <AchievementsView>
-        <ListButton buttonText={'Друг Достоевского'} type={'achievements'} percent={85}/>
-        <ListButton buttonText={'Петербургская интеллигенция'} type={'achievements'} percent={30}/>
-        <ListButton buttonText={'История наводнений в Санкт-Петербурге'} type={'achievements'} percent={70}/>
-        <ListButton buttonText={'Начало положено!'} type={'achievements'} percent={100}/>
-      </AchievementsView>
+      <AchievementsFlatList
+        data={
+          currentTab === 1 ? achievementsList
+            : currentTab === 2 ? achievementsList.filter(achievement => achievement.progress === 100)
+              : achievementsList.filter(achievement => achievement.progress < 100)
+        }
+        renderItem={({ item }): React.ReactElement => (
+          <ListButton
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            buttonText={item.name}
+            type={'achievements'}
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            percent={item.progress}
+          />
+        )}
+        keyExtractor={(item, index): string => index.toString()}
+        ListEmptyComponent={<BasicText>Empty</BasicText>}
+      />
     </Body>
   );
 }
