6635b5f4f6a7502c2c1f31c2ba07a75d00f71ab2
Panda0412 <nasb_99@mail.ru>
2021-06-15 18:37:09 +0300

    Set fetchPolicy to 'store-and-network'

--- src/screens/Friends.tsx
+++ src/screens/Friends.tsx
@@ -106,7 +106,8 @@ export default function FriendsScreen({ navigation }: Props): React.ReactElement
         }
       }
     `,
-    {}
+    {},
+    { fetchPolicy: 'store-and-network' }
   );
 
   return (
