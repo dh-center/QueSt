2fefa12776ce7547faca309dac29c598d00ef5a7
Nikita Melnikov <nikmel2803@gmail.com>
2021-06-23 01:39:06 +0300

    remove unnecessary line

--- android/app/src/main/java/ru/dhcenter/quest/MainApplication.java
+++ android/app/src/main/java/ru/dhcenter/quest/MainApplication.java
@@ -28,7 +28,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-//          packages.add(new RNSoundPackage());
           return packages;
         }
 
