d4c7ea4abbfc217fcfbaa4f9458c0309d4e7c84d
Panda0412 <nasb_99@mail.ru>
2021-06-22 03:27:18 +0300

    changing avatar

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StackScreenProps } from '@react-navigation/stack';
 import { ProfileStackParamList } from '../navigation/profileStack';
@@ -10,6 +10,13 @@ import BlueCircle15 from '../images/blueCircle15.svg';
 import { StyledFonts } from '../styles/textStyles';
 import Avatar from '../components/Avatar';
 import BackArrow from '../components/BackArrow';
+import { Asset, launchImageLibrary } from 'react-native-image-picker';
+import { Alert, Modal, Platform } from 'react-native';
+import { API_ENDPOINT } from '@env';
+import { useMutation } from 'react-relay/hooks';
+import { graphql } from 'react-relay';
+import { SettingsMutation } from './__generated__/SettingsMutation.graphql';
+import Button from '../components/ui/Button';
 
 /**
  * Type with props of screen 'Settings' in ProfileStackScreen
@@ -95,6 +102,27 @@ const LogoutText = styled.Text`
   color: ${Colors.White};
 `;
 
+const ModalView = styled.TouchableOpacity`
+  background-color: rgba(85,85,107,0.8);
+  flex: 1;
+  padding: 74px 15px;
+  align-items: center;
+`;
+
+const ModalImageView = styled.TouchableOpacity`
+  background-color: ${Colors.Background};
+  border-radius: 15px;
+  elevation: ${8};
+  box-shadow: 0 4px 4.65px rgba(0,0,0,0.2);
+  margin-bottom: 30px;
+`;
+
+const ModalImage = styled.Image`
+  width: 100%;
+  aspect-ratio: 1;
+  border-radius: 15px;
+`;
+
 /**
  * Displays settings screen
  *
@@ -103,6 +131,64 @@ const LogoutText = styled.Text`
 export default function SettingsScreen({ route, navigation }: Props): React.ReactElement {
   const { t } = useTranslation();
   const authContext = useAuthContext();
+  const [newAvatar, setNewAvatar] = React.useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [ updateAvatar ] = useMutation<SettingsMutation>(
+    graphql`
+      mutation SettingsMutation($avatar: String!) {
+        user {
+          update(input: {photo: $avatar}) {
+            record {
+              id
+              username
+              photo
+            }
+          }
+        }
+      }
+    `
+  );
+
+  /**
+   * Image uploader
+   *
+   * @param response - image from user device
+   */
+  const imageUpload = async (response: Asset): Promise<void> => {
+    try {
+      const formData = new FormData();
+
+      formData.append('image', {
+        name: response.fileName,
+        filename: response.fileName,
+        type: response.type,
+        uri:
+          Platform.OS === 'android' ? response.uri : response.uri.replace('file://', ''),
+      });
+
+      const res = await fetch(
+        `${API_ENDPOINT}/upload/user`,
+        {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      const responseJson = await res.json();
+
+      if (responseJson.success === 1) {
+        setNewAvatar(responseJson.file.url);
+        setModalVisible(true);
+      }
+    } catch (e) {
+      Alert.alert(t([`errors.${e.message}`, 'errors.unspecific']));
+    }
+  };
 
   return (
     <Wrapper>
@@ -125,12 +211,41 @@ export default function SettingsScreen({ route, navigation }: Props): React.Reac
 
       <Body>
         <ListButton buttonText={t('settings.password')} type={'settings'}/>
-        <ListButton buttonText={t('settings.avatar')} type={'settings'}/>
+        <ListButton
+          buttonText={t('settings.avatar')}
+          type={'settings'}
+          onPress={() => launchImageLibrary({ mediaType: 'photo',
+            includeBase64: true }, resp => {
+            // setResponse(resp.assets[0] || null);
+            resp.didCancel ? console.log('cancelled') : imageUpload(resp.assets[0]);
+          })}/>
         <ListButton buttonText={t('settings.about')} type={'info'} onPress={() => navigation.navigate('About')}/>
         <LogoutButton onPress={(): Promise<void> => authContext.actions.logout()}>
           <LogoutText>{t('settings.logout')}</LogoutText>
         </LogoutButton>
       </Body>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={(): void => setModalVisible(false)}
+        statusBarTranslucent={true}
+      >
+        <ModalView activeOpacity={1} onPress={() => setModalVisible(false)}>
+          <ModalImageView activeOpacity={1}>
+            <ModalImage source={{ uri: newAvatar }}/>
+          </ModalImageView>
+          <Button title={t('settings.confirm')} onPress={() => {
+            updateAvatar({
+              variables: { avatar: newAvatar },
+              onError: () => Alert.alert(t('errors.unspecific')),
+            });
+            setModalVisible(false);
+            navigation.navigate('Main');
+          }}/>
+        </ModalView>
+      </Modal>
     </Wrapper>
   );
 }
